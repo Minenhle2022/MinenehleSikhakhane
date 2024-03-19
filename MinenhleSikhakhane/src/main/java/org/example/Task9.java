02c4e32b40de3bb5a1ebc111df246cd0797e99eb
Minenhle <minenhleskhy@gmail.com>
2024-03-19 23:00:03 +0200

    second commit

--- MinenhleSikhakhane/src/main/java/org/example/Task9.java
+++ MinenhleSikhakhane/src/main/java/org/example/Task9.java
@@ -3,5 +3,18 @@ package org.example;
 public class Task9 {
     public static void main(String[] args) {
 
+        int [] number ={12,34,22,88,17,5,3,10};
+        int i=0;
+        int j=0;
+
+        for(int num :number){
+            if (i<num){
+                j=i;
+                i=num;
+            } else if (j<num) {
+                j=num;
+            }
+        }
+
     }
 }
